59b13384200d20e348360de99b8efb73e86a6ee8
Willian Damasceno <williandamascenoprofessional@gmail.com>
2024-05-25 12:24:11 -0300

    Vaccine routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function (Request $request) {
     return response()->json(['message' => 'Hello World!']);
 });
+
+Route::group(['prefix' => 'vaccine'], function () {
+    Route::post('/cat', function (Request $request) {
+        return response()->json('cat');
+    });
+
+    Route::post('/dog', function (Request $request) {
+        return response()->json('dog');
+    });
+});
